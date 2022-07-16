cbe184d0be4ff8c9ca7882ded9dc25ba01f86b7c
bllakcn <bllakcn35@gmail.com>
2022-07-16 19:56:49 +0200

    game completed

--- main.js
+++ main.js
@@ -13,13 +13,50 @@ function getComputerChoice() {
     return result;
 };
 
-const computer = getComputerChoice();
-const user = prompt("Enter a choice: ").toUpperCase();
-
-function main (computer , user) {
-    console.log(user);
-    console.log(computer);
-     
+function round (computer , user) {
+    let result;
+    if(user === "ROCK"){
+        if(computer === "PAPER"){
+            result = "fail";
+        }
+        if(computer === "SCISSORS"){
+            result = "win";
+        }
+        if(computer === "ROCK"){
+            result = "tie";
+        }
+    };
+    if(user === "PAPER"){
+        if(computer === "PAPER"){
+            result = "tie";
+        }
+        if(computer === "SCISSORS"){
+            result = "fail";
+        }
+        if(computer === "ROCK"){
+            result = "win";
+        }
+    };
+    if(user === "SCISSORS"){
+        if(computer === "PAPER"){
+            result = "win";
+        }
+        if(computer === "SCISSORS"){
+            result = "tie";
+        }
+        if(computer === "ROCK"){
+            result = "fail";
+        }
+    };
+    return result;
 };
 
-main(computer, user);
+function game(){
+    for(let i = 0; i < 5; i++){
+        const computer = getComputerChoice();
+        const user = prompt("Enter a choice: ").toUpperCase();
+        console.log(round(computer, user));
+    }
+};
+
+game();
